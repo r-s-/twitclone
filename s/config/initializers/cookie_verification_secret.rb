d7295e80060dcf1a57f4d1783881bb570d309a4e
Ryan Selk <ryanselk@gmail.com>
2014-02-13 03:39:35 -0700

    add ssl for production

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '1305995bfd02328c47130f7cb33204b5b36c7eb2fec65fad6a50bd2ea62952672111f8a6fe6486204bd460834fde583f32020611808b11d5570b4dd692ae72cb';
