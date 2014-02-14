156336ff7ba23cb8465a06e9ea40e61574656867
Ryan Selk <ryanselk@gmail.com>
2014-02-14 01:04:35 -0700

    before trying to change helper includes with guard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def show
     @user = User.find(params[:id])
   end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,26 @@ describe "User pages" do
 
   subject { page }
 
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user)}
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile")}
+      it { should have_title("Edit user")}
+      it { should have_link('change', href: 'http://gravatar.com/emails')}
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes"}
+
+      it { should have_content('error')}
+    end
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
